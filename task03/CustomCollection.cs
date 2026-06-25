c5ef5d8343a20048283b4085a027fec41b02b44f
Your Name <vladkomlev10@gmail.com>
2026-06-26 05:45:55 +0600

    task03 w/ tests

---
+++ task03/CustomCollection.cs
@@ -0,0 +1,33 @@
+﻿﻿using System.Linq;
+using System.Collections;
+
+namespace task03;
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    public void Remove(T item) => _items.Remove(item);
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerable <T> GetReverseEnumerator() 
+    {
+        for (int i = _items.Count-1; i >= 0; i--)
+        {
+            yield return _items[i];
+        }
+    }
+    public static IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        for (int i = 0; i < count; i ++)
+        {
+            yield return start + i;
+        }
+    }
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        return _items.Where(predicate).OrderBy(keySelector);
+    }
+}
