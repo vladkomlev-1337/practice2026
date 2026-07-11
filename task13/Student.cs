e2575b33347d5615f9ad94cb1ac1d8662add3c8e
Your Name <vladkomlev10@gmail.com>
2026-07-11 21:37:19 +0600

    feat: task13 w tests

---
+++ task13/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace task13;
+
+public class Subject
+{
+  public string Name {get; set; }
+  public int Grade {get; set; }
+}
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [JsonConverter(typeof(DateConverter))]
+    public DateTime BirthDate { get; set; }
+    public List<Subject> Grades { get; set; }
+}
