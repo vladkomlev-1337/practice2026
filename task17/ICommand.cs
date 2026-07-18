601ae22abcd24d04678b8ce5103f73b1e2633c52
Your Name <vladkomlev10@gmail.com>
2026-07-18 06:21:20 +0600

    task19 with graph and report

--- task17/ICommand.cs
+++ task17/ICommand.cs
@@ -4,3 +4,20 @@ public interface ICommand
 {
     void Execute();
 }
+public class TestCommand : ICommand
+{
+    private readonly int _id;
+    private int _counter = 0;
+
+    public TestCommand(int id)
+    {
+        _id = id;
+    }
+
+    public int Counter => _counter;
+
+    public void Execute()
+    {
+        Console.WriteLine($"Поток {_id} вызов {++_counter}");
+    }
+}
